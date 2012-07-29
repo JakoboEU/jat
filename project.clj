26c3d2f0a83c05ef7b9dca8970eb5c6d8d3d3783
James <james@dino.(none)>
2012-07-29 20:31:01 +0100

    Updating to use released version of junit test classes

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "Library for core functionality of Leiningen."
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [congomongo/congomongo "0.1.7"]
+                 [congomongo/congomongo "0.1.9"]
                  [org.clojure/algo.monads "0.1.0"]
 		 [junit/junit "4.10"]
-		 [jat/jat-testdata "0.0.1-SNAPSHOT"]])
+		 [jat/jat-testdata "1.0.0"]])
